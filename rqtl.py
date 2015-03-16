249fc3161b225126ac8b90fa3775e05137aab847
DannyArends <Danny.Arends@gmail.com>
2015-03-16 20:32:06 +0100

    Plot to PNG

--- rqtl.py
+++ rqtl.py
@@ -7,34 +7,55 @@
 from numpy import *
 from pandas import *
 
-import scipy as sp                        # SciPy
-import rpy2.robjects as ro                # R Objects
-import pandas.rpy.common as com           # R common functions
-
+import scipy as sp                          # SciPy
+import rpy2.robjects as ro                  # R Objects
+import pandas.rpy.common as com             # R common functions
 
 ## Get pointers to some common R functions
-library = ro.r["library"]                 # Map the library function
-plot        = ro.r["plot"]                # Map the plot function
-c           = ro.r["c"]                   # Map the c function
-data        = ro.r["data"]                # Map the data function
+r_library     = ro.r["library"]             # Map the library function
+r_plot        = ro.r["plot"]                # Map the plot function
+r_c           = ro.r["c"]                   # Map the c function
+r_class       = ro.r["class"]               # Map the class function
+r_list        = ro.r["list"]                # Map the list function
+r_data        = ro.r["data"]                # Map the data function
+r_dim         = ro.r["dim"]                 # Map the dim function
 
-print(library("qtl"))                     # Load R/qtl
+r_png         = ro.r["png"]                 # Map the dim function
+r_off         = ro.r["dev.off"]                 # Map the dim function
+
+print(r_library("qtl"))                     # Load R/qtl
 
 ## Get pointers to some R/qtl functions
-scanone     = ro.r["scanone"]             # Map the scanone function
-read_cross  = ro.r["read.cross"]          # Map the read.cross function
+scanone     = ro.r["scanone"]               # Map the scanone function
+read_cross  = ro.r["read.cross"]            # Map the read.cross function
 
-## Test1, use a dataset in R/qtl
-res         = data("multitrait")          # Load the multitrait dataset
-data1       = ro.r["multitrait"]          # Get a pointer to it
+## Test1 - Use a dataset in R/qtl
+res         = r_data("multitrait")          # Load the multitrait dataset
+data1       = ro.r["multitrait"]            # Get a pointer to it
 
 results1 = scanone(data1)
-plot(results1)
+r_plot(results1)
 
-## Test2, use a dataset from HDD
-data2 = read_cross(file = "data/multi.data.csv", format = "csvr", genotypes=c("AA","BB"))
+## Test2 - Use a dataset from the HDD
+data2 = read_cross(file = "data/multi.data.csv", format = "csvr", genotypes = r_c("AA","BB"))
 results2 = scanone(data2)
-plot(results2)
+r_plot(results2)
+
+## Test3 - Construct a python cross object and send it to R
+pydata      = DataFrame.from_csv("data/multi.data.csv", -1)
+
+phenotypes    = pydata[isnan(pydata[1])].iloc[:,range(2, max(pydata.columns))]
+rphenotypes   = com.convert_to_r_dataframe(phenotypes)
+chr1          = pydata[pydata[1] == 1].iloc[:,range(2, max(pydata.columns))]
+rchr1         = com.convert_to_r_dataframe(chr1)
+map1          = pydata[pydata[1] == 1].iloc[:,range(0, 2)]
+rmap1         = com.convert_to_r_dataframe(map1)
+
+rgeno  = r_list(r_list(data = rchr1,map =  rmap1))
+rcross = r_list(pheno = rphenotypes,geno = rgeno)
+ro.globalenv["rcross"] = rcross
+ro.r('class(rcross) = c("riself","cross")')                 # Update the class to be cross and riself
+rcross       = ro.r["rcross"]                               # Get a pointer to it
 
 quit()
 
